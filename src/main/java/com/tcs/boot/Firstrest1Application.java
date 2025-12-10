94e5c7bd755efe787acf3ea66c693abf23efe664
ShettyHimasree <himasreeshetty@gmail.com>
2025-12-10 06:39:02 +0000

    first commit

---
+++ src/main/java/com/tcs/boot/Firstrest1Application.java
@@ -0,0 +1,14 @@
+package com.tcs.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Firstrest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Firstrest1Application.class, args);
+		System.out.println("asdfghj");
+	}
+
+}
